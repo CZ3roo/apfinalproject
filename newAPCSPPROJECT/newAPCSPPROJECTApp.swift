c7740d70e03f0b6fb8ba957ddc3d98bd3d91a58d
czhao24 <usachriszhao@gmail.com>
2023-03-26 21:29:31 -0500

    Initial Commit

---
+++ newAPCSPPROJECT/newAPCSPPROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  newAPCSPPROJECTApp.swift
+//  newAPCSPPROJECT
+//
+//  Created by Christopher Zhao24 on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct newAPCSPPROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
